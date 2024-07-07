681f054a3dae1649f275f291850d2ca8b3954a81
Pratik Somwanshi <pratiksomwanshi570@gmail.com>
2024-07-07 16:49:57 +0530

    done with user registration authentication

--- controller/authController.js
+++ controller/authController.js
@@ -1,13 +1,32 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { User } = require("../model");
+const { ErrorResponse } = require("../utils");
 
 const register = async (req, res) => {
     const { username, password } = req.body;
+
+    const existingUser = await User.findOne({
+        where: {
+            username,
+        },
+    });
+
+    if (existingUser) {
+        ErrorResponse.error = {
+            explanation: "User already registered",
+        };
+        return res.status(404).json(ErrorResponse);
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({ username, password: hashedPassword });
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+
     res.json({
         username: user.username,
+        token,
     });
 };
 
